2dd7e2935ddc78a56b362fbcb2cc7f1d9a7b9b33
Jaakko Toivonen <jaakkotoivonen@88-113-235-24.elisa-laajakaista.fi>
2021-08-07 19:07:57 +0300

    split to separate seasons, 21 pre-season processing

---
+++ season21_22/py_files/fbref_data.py
@@ -0,0 +1,31 @@
+"""
+Web scraper for fetching player data from fbref.com
+"""
+
+import pandas
+import requests
+
+def fetch_player_data():
+    
+    reply = requests.get("https://fbref.com/en/comps/9/stats/Premier-League-Stats")
+    
+    # remove strings that make part of the HTML into a comment section
+    # the relevant part is commented out, so removing comment strings makes them "visible" again
+    my_text = reply.text.replace('<!--', '')
+    my_text = my_text.replace('-->', '')
+
+    # now we can read the relevant table with read_html
+    data_list = pandas.read_html(my_text)
+    players = data_list[2]
+    
+    # edit column names that have unnamed main headers
+    new_columns = [('General',col[1]) if 'Unnamed' in col[0] \
+                   else col for col in players.columns]
+    players.columns = pandas.MultiIndex.from_tuples(new_columns)
+    # remove unnecessary rows
+    ix_to_remove = players[players['General']['Squad']=='Squad'].index
+    players.drop(ix_to_remove, inplace=True)
+    # fix dtypes
+    players = players.apply(lambda col:pandas.to_numeric(col,errors='ignore'))
+    
+    return players
