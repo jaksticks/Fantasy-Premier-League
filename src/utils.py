63f77117fbe5f5bfeb879ac236e5cc99cb5f5b67
jaksticks <jaksticks@gmail.com>
2023-08-31 10:50:34 +0300

    updates on whole pipeline, added model evaluation

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,11 @@ import numpy as np
 from pathlib import Path
 import os
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+import plotly.express as px
+import plotly.graph_objects as go
+
 def fetch_latest_fpl_data(folder_path_str: str = '../data/fpl/') -> pd.DataFrame:
     '''Fetch most recent saved FPL data.'''
     
@@ -16,3 +21,31 @@ def fetch_latest_fpl_data(folder_path_str: str = '../data/fpl/') -> pd.DataFrame
     full_path = folder_path_str + file
     latest_data = pd.read_csv(full_path, index_col=0)
     return latest_data
+
+def calculate_performance_metrics(y_true, y_predicted, plot=True):
+    '''Calculate test metrics for regression (r2, mae, mse).'''
+    mae = mean_absolute_error(y_true, y_predicted)
+    rmse = mean_squared_error(y_true, y_predicted, squared=False)
+    r2 = r2_score(y_true, y_predicted)
+
+    if plot:
+        x0 = y_predicted
+        y0 = y_true
+
+        fig = px.scatter(
+            x=x0, 
+            y=y0, 
+            marginal_x="histogram", 
+            marginal_y="histogram",
+            labels={'x':'expected points', 'y': 'actual points'},
+            )
+        
+        fig.add_trace(
+            go.Scatter(x=np.linspace(np.min(x0), np.max(x0), 100), 
+                    y=np.linspace(np.min(x0), np.max(x0), 100),
+                    showlegend=False,)
+        )
+
+        fig.show()
+
+    return (mae, rmse, r2)
