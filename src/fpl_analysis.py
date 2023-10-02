3bc1bf01cd73ef15a597cc90ec585614696a2ebc
jaksticks <jaksticks@gmail.com>
2023-10-02 22:37:29 +0300

    add fix for double gameweek processing

--- src/fpl_analysis.py
+++ src/fpl_analysis.py
@@ -325,6 +325,8 @@ def data_processing(season_folder: str, shift_param: int = 1):
         team = row.team_name
         season = row.season
         games = fixtures_melt[(fixtures_melt.value==team) & (fixtures_melt.event==gameweek) & (fixtures_melt.season==season)]
+        # if there are multiple games for the gameweek, take the last one before new data was updated
+        games = games[games.kickoff_time < row.data_retrieved_datetime].tail(1)
         if games.shape[0]!=1:
             team_data.append( np.array([np.nan]*nr_cols) )
             opponent_data.append( np.array([np.nan]*nr_cols) )
