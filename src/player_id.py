9c044f51c50619e7a6567d6017f48735e731d022
jaksticks <jaksticks@gmail.com>
2026-01-01 11:15:46 +0200

    update before week 19

--- src/player_id.py
+++ src/player_id.py
@@ -3,21 +3,36 @@ import pandas as pd
 from pathlib import Path
 from projections_for_optimization import initial_projection_data
 
-def main(player_name, latest_gameweek, current_season, season_folder):
+def main(args, current_season, season_folder):
     
-    projections, _ = initial_projection_data(latest_gameweek, current_season, season_folder)
+    projections, _ = initial_projection_data(args.latest_gameweek, current_season, season_folder)
     
-    # Find player ids matching given string
-    result = (projections.loc
-        [projections['name'].str.contains(player_name), ['name', 'team_name', 'id']]
-        .drop_duplicates()
-    )
+    if args.player_name:
+        # Find player ids matching given string
+        result = (projections.loc
+            [projections['name'].str.contains(args.player_name), ['name', 'team_name', 'id']]
+            .drop_duplicates()
+        )
 
-    # Check if the player ID exists in the data
-    if result.empty:
-        print(f"No string match for {player_name}")
+        # Check if the player ID exists in the data
+        if result.empty:
+            print(f"No string match for {args.player_name}")
+        else:
+            print(result)
+    elif args.player_ids:
+        # Find player names matching given ids
+        result = (projections.loc
+            [projections['id'].isin(args.player_ids), ['name', 'team_name', 'id']]
+            .drop_duplicates()
+        )
+
+        # Check if the player IDs exist in the data
+        if result.empty:
+            print(f"No matches for given player IDs: {args.player_ids}")
+        else:
+            print(result)
     else:
-        print(result)
+        print("Please provide either --player_name or --player_ids argument.")
 
 if __name__ == "__main__":
 
@@ -29,9 +44,14 @@ if __name__ == "__main__":
         help="Ongoing gameweek, i.e., not the gameweek for the next deadline."
     )
     parser.add_argument(
-        "player_name",
+        "--player_name",
         type=str,
         help="Player name to match."
+    )
+    parser.add_argument(
+        "--player_ids",
+        type=lambda x: [int(i) for i in x.split(',')],
+        help="Comma-separated list of player IDs."
     )
     args = parser.parse_args()
 
@@ -40,4 +60,4 @@ if __name__ == "__main__":
     SEASON_FOLDER = config_json['SEASON_FOLDER']
     current_season = SEASON_FOLDER[-5::].replace('_','-')
 
-    main(args.player_name, args.latest_gameweek, current_season, SEASON_FOLDER)
+    main(args, current_season, SEASON_FOLDER)
